462516ae47071d4df2b3950be6a3149eafc5daaf
Jason Park <jjpark987@gmail.com>
2022-09-30 14:57:53 -0700

    added pictures and dates

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-//const h2 = document.createElement("h2");
-//h2.textContent = "This content added by JavaScript";
+for (let i = 0; i < 9; i++) {
+    const div = document.createElement(`div`);
+    
+    div.setAttribute(`id`, `sep${i + 3}`);
 
-//document.querySelector("body").appendChild(h2);
+    document.querySelector(`body`).appendChild(div);
+
+    const image = document.createElement(`img`); 
+    
+    image.setAttribute(`src`, `./pictures/sep${i + 3}.jpeg`);
+    image.setAttribute(`alt`, `sep${i + 3}`);
+    if (i + 3 === 6 || i + 3 === 8 || i + 3 === 11) {
+        image.setAttribute(`width`, `auto`);
+        image.setAttribute(`height`, `145`);
+    } else {
+        image.setAttribute(`width`, `145`);
+        image.setAttribute(`height`, `auto`);
+    }
+    
+    document.getElementById(`sep${i + 3}`).appendChild(image);
+
+    const h2 = document.createElement(`h2`);
+    
+    h2.textContent = `September ${i + 3}`;
+
+    document.getElementById(`sep${i + 3}`).appendChild(h2);
+}
