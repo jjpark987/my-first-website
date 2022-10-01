347f0604c65f2fba91ae51ae9b4a3c737e7d3de3
Jason Park <jjpark987@gmail.com>
2022-09-30 19:51:53 -0700

    title and timeline

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const timeline = document.createElement(`timeline`);
+
+timeline.setAttribute(`class`, `timeline`);
+
+document.body.appendChild(timeline);
+
 for (let i = 0; i < 9; i++) {
     const div = document.createElement(`div`);
     
@@ -11,9 +17,9 @@ for (let i = 0; i < 9; i++) {
     image.setAttribute(`alt`, `sep${i + 3}`);
     if (i + 3 === 6 || i + 3 === 8 || i + 3 === 11) {
         image.setAttribute(`width`, `auto`);
-        image.setAttribute(`height`, `145`);
+        image.setAttribute(`height`, `100`);
     } else {
-        image.setAttribute(`width`, `145`);
+        image.setAttribute(`width`, `100`);
         image.setAttribute(`height`, `auto`);
     }
     
@@ -24,4 +30,7 @@ for (let i = 0; i < 9; i++) {
     h2.textContent = `September ${i + 3}`;
 
     document.getElementById(`sep${i + 3}`).appendChild(h2);
+
+    document.querySelector(`timeline`).appendChild(div);
 }
+
